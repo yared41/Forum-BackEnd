5412f54a84be62e18eddac074722a9d254d8c522
yared41 <124960304+yared41@users.noreply.github.com>
2023-05-23 20:38:59 -0500

    first commit

---
+++ server/config/database.js
@@ -0,0 +1,91 @@
+require("dotenv").config();
+const mysql = require('mysql2');
+
+const pool = mysql.createPool({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.MYSQL_DB,
+  connectionLimit: 10,
+socketPath: "/Applications/MAMP/tmp/mysql/mysql.sock"
+});
+
+
+
+
+pool.getConnection(function (err, connection) {
+  console.log("database connected");
+});
+
+//   ==================== create registration table ======================
+
+let registration = `CREATE TABLE IF NOT EXISTS registration(
+    user_id int auto_increment,
+    user_name varchar(255) not null,
+    user_email varchar(255) not null,
+    user_password varchar(255) not null,
+    PRIMARY KEY (user_id)
+
+    )`;
+
+pool.query(registration, (err, results) => {
+  if (err) throw err;
+  console.log("registration table created");
+});
+
+// //   ==================== create profile table ======================
+
+let profile = `CREATE TABLE IF NOT EXISTS profile(
+    user_profile_id int auto_increment,
+    user_id int not null,
+    first_name varchar(255) not null,
+    last_name varchar(255) not null,        
+    PRIMARY KEY (user_profile_id),
+    FOREIGN KEY (user_id) REFERENCES registration(user_id)
+)`;
+
+pool.query(profile, (err, results) => {
+  if (err) throw err;
+  console.log("profile table created");
+});
+
+// //   ==================== create question table ======================
+
+let question = `CREATE TABLE IF NOT EXISTS question(
+    question_id int auto_increment,
+    question varchar(255) not null,
+    question_description varchar(255),
+    question_code_block varchar(255),
+    tags varchar(255),
+    post_id varchar(255) not null,
+    user_id int not null,
+    PRIMARY KEY (question_id),
+    UNIQUE KEY (post_id),
+    FOREIGN KEY (user_id) REFERENCES registration(user_id)
+)`;
+
+pool.query(question, (err, results) => {
+  if (err) throw err;
+  console.log("question table created");
+});
+
+// // //   ==================== create answer table ======================
+
+let answer = `CREATE TABLE IF NOT EXISTS answer(
+    answer_id int auto_increment,
+    answer varchar(255) not null,
+    answer_code_block varchar(255),
+    user_id int not null,
+    question_id int not null,
+    PRIMARY KEY (answer_id),
+    FOREIGN KEY (user_id) REFERENCES registration(user_id),
+    FOREIGN KEY (question_id) REFERENCES question(question_id)
+)`;
+
+pool.query(answer, (err, results) => {
+  if (err) throw err;
+  console.log("answer table created");
+})
+
+
+module.exports = pool;
